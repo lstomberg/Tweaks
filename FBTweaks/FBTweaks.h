87d7412f20d2ab98ad4db9682c23f4bfd8085e8a
Lucas Stomberg <lstomberg@gmail.com>
2018-01-23 12:00:56 -0600

    Create .framework target and shared scheme to compile the framework.  This allows compatibility with Carthage.

---
+++ FBTweaks/FBTweaks.h
@@ -0,0 +1,19 @@
+//
+//  FBTweaks.h
+//  FBTweaks
+//
+//  Created by Lucas Stomberg on 1/23/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FBTweaks.
+FOUNDATION_EXPORT double FBTweaksVersionNumber;
+
+//! Project version string for FBTweaks.
+FOUNDATION_EXPORT const unsigned char FBTweaksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FBTweaks/PublicHeader.h>
+
+
